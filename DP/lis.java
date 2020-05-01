02eece92bcfeecccf9fd505934054c3970d21c47
Mujahid Jaffer <mujahidrj@gmail.com>
2020-05-01 03:43:23 -0400

    started lis but not done

---
+++ DP/lis.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class lis {
+  public static int lengthofLIS(int[] nums) {
+    return 0;
+  }
+
+  public static void main(String[] args) {
+    int[] arr = new int[] { 10, 9, 2, 5, 3, 4 };
+    System.out.println(lengthofLIS(arr));
+  }
+}
